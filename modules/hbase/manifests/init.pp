40c5e257976c8694acccb17c2570c1f7d4e07e19
André Kelpe <efeshundertelf@googlemail.com>
2015-04-24 15:05:05 +0200

    Use HBase 0.98.12

--- modules/hbase/manifests/init.pp
+++ modules/hbase/manifests/init.pp
@@ -1,5 +1,5 @@
 class hbase {
-  $hbase_version = "0.98.11"
+  $hbase_version = "0.98.12"
   $hbase_platform = "hadoop2"
   $hbase_home = "/opt/hbase-${hbase_version}-${hbase_platform}"
   $hbase_tarball = "hbase-${hbase_version}-${hbase_platform}-bin.tar.gz"
